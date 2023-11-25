eb463ea8399479d963f19f7301cc7197cdaec1fe
cornL <kornel.havasi77@gmail.com>
2023-11-25 15:31:36 +0100

    knockback added

--- scenes/LS_hitbox.cs
+++ scenes/LS_hitbox.cs
@@ -1,9 +1,13 @@
 using Godot;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public partial class LS_hitbox : Area3D
 {
 	public float damage = 20f;
+	enemyMovementBaseClas enemyBody;
 	private void _on_area_entered(Area3D area)
 	{
 		GD.Print("nahhhhhhh");
@@ -11,7 +15,22 @@ public partial class LS_hitbox : Area3D
 		{
 			GD.Print("Hit registered");
 			var enemy = area as enemyHitboxBaseClass;
+			enemyBody = area.GetParent() as enemyMovementBaseClas;
+			enemyBody.isHit = true;
+			enemyBody.Velocity = Vector3.Zero;
+			enemyBody.Velocity -= 20*(GetParent().GetParent().GetParent().GetParent().GetParent().GetParent().GetParent<Node3D>().GlobalTransform.Origin - enemyBody.GlobalTransform.Origin);
 			enemy.hp -= damage;
+			DelayMethod(0.08f);
+			
+			GD.Print("AAAAAAAAAAAAAAAAAAAA");
+
 		}
 	}
+	
+	private async void DelayMethod(float amount)
+	{
+		await ToSignal(GetTree().CreateTimer(amount), "timeout");
+		enemyBody.isHit = false;
+	}
+
 }
--- scenes/enemyMovementBaseClas.cs
+++ scenes/enemyMovementBaseClas.cs
@@ -6,6 +6,7 @@ public partial class enemyMovementBaseClas : CharacterBody3D
 	public int speed = 5;
 	public int gravity = 15;
 	public Node3D player;
+	public bool isHit = false;
 	
 	public override void _Ready()
 	{
@@ -16,13 +17,15 @@ public partial class enemyMovementBaseClas : CharacterBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 targetDirection = (player.GlobalTransform.Origin - GlobalTransform.Origin).Normalized();
-		Velocity = new Vector3(targetDirection.X * speed, Velocity.Y,targetDirection.Z * speed);
+		if(!isHit)
+			Velocity = new Vector3(targetDirection.X * speed, Velocity.Y,targetDirection.Z * speed);
+		
 		if(!IsOnFloor())
 		{
 			Velocity = new Vector3(Velocity.X,Velocity.Y - (gravity * (float)delta), Velocity.Z);
 		}
 		
-		GD.Print(Velocity);
+		GD.Print();
 		MoveAndSlide();
 	}
 }
